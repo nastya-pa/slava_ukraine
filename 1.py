bc8ac924ee6efc6a919577d36f8288e4acff6f9d
Настя Пахнюк <anastasia.221004@gmail.com>
2021-04-03 11:44:28 +0300

    p

--- 1.py
+++ 1.py
@@ -1,25 +1,25 @@
 field = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
 
 
-def out_field(field):
-    print('-------------')
-    a = ''
+def out_field11(field):
+    print('----------')
+    a1 = ''
     for i in field:
         for j in i:
-            a += '| '
+            a1 += '| '
             a += j
             a += ' '
-        a += '|'
-        print(a)
+        a1 += '|'
+        print(a1)
         a = ''
     print('-------------')
     return '\n'
 
 
-print(out_field(field))
+print(out_field11(field))
 
 
-def out_field(field):
+def out_field(fielt):
     print('-------------')
     a = ''
     for i in field:
