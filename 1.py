0bbd60068ba49fb8761fc828f3a7561e84cf0907
Настя Пахнюк <anastasia.221004@gmail.com>
2021-04-03 11:41:13 +0300

    a

---
+++ 1.py
@@ -0,0 +1,34 @@
+field = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+
+
+def out_field(field):
+    print('-------------')
+    a = ''
+    for i in field:
+        for j in i:
+            a += '| '
+            a += j
+            a += ' '
+        a += '|'
+        print(a)
+        a = ''
+    print('-------------')
+    return '\n'
+
+
+print(out_field(field))
+
+
+def out_field(field):
+    print('-------------')
+    a = ''
+    for i in field:
+        for j in i:
+            a += '| '
+            a += j
+            a += ' '
+        a += '|'
+        print(a)
+        a = ''
+    print('-------------')
+    return '\n'
